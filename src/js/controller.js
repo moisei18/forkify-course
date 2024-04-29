a7625f3068d51a8565a1b913fa47e0a209ef3451
moisei18 <moisei118@gmail.com>
2024-04-29 13:08:42 +0200

    Add new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -156,6 +156,10 @@ logoBtn.addEventListener(
   () => (window.location.href = 'https://forkify-alexmo.netlify.app/')
 );
 
+const newFeature = function () {
+  console.log('Welcome to the application!');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -164,7 +168,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('welcome');
+  newFeature();
 };
 init();
 
